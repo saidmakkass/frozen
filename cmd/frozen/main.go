508d95452d0c82d9f6704a728fbec038f8a8607d
saidmakkass <saidmakkass1@gmail.com>
2026-09-08 04:05:50 +0100

    feat: Add better error handling

--- cmd/frozen/main.go
+++ cmd/frozen/main.go
@@ -11,19 +11,18 @@ func getAddr(args []string) (addr string) {
 	const defaultPrefix = "127.0.0.1"
 	lenArgs := len(args)
 	if lenArgs != 2 && lenArgs != 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [address] <port>\n", args[0])
-		os.Exit(1)
+		panic(fmt.Sprintf("Usage: %s [address] <port>\n", args[0]))
 	}
 	if lenArgs == 2 {
 		_, err := strconv.Atoi(args[1])
 		if err != nil {
-			panic(fmt.Sprintf("Invalid Port %s\n", args[1]))
+			panic(fmt.Sprintf("Invalid Port %q", args[1]))
 		}
 		addr = fmt.Sprintf("%s:%s", defaultPrefix, args[1])
 	} else {
 		_, err := strconv.Atoi(args[2])
 		if err != nil {
-			panic(fmt.Sprintf("Invalid Port %s\n", args[2]))
+			panic(fmt.Sprintf("Invalid Port %q", args[2]))
 		}
 		addr = fmt.Sprintf("%s:%s", args[1], args[2])
 	}
@@ -31,6 +30,11 @@ func getAddr(args []string) (addr string) {
 }
 
 func main() {
+	defer func() {
+        if r := recover(); r != nil {
+            fmt.Fprintf(os.Stderr, "%s\n", r)
+        }
+    }()
 	addr := getAddr(os.Args)
 	server.RunServer(addr)
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -8,13 +8,13 @@ import (
 func RunServer(addr string) net.Listener {
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
-		panic(fmt.Sprintf("Error listening on %s\n", addr))
+		panic(fmt.Sprintf("Error listening on %s", addr))
 	}
 	fmt.Printf("Listening on %s\n", addr)
 	for {
 		con, err := ln.Accept()
 		if err != nil {
-			panic(fmt.Sprintf("Error during connection from %s\n", con.RemoteAddr()))
+			panic(fmt.Sprintf("Error during connection from %s", con.RemoteAddr()))
 		}
 		go handleConnection(con)
 	}
