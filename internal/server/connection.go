dead5f7c190184b2373e505abbb8db91047726d4
saidmakkass <saidmakkass1@gmail.com>
2026-09-08 06:48:37 +0100

    feat: Add basic data reading

--- internal/server/connection.go
+++ internal/server/connection.go
@@ -3,8 +3,18 @@ package server
 import (
 	"net"
 	"fmt"
+	"bufio"
 )
 
 func handleConnection(con net.Conn) {
-	fmt.Printf("Handling connection from %s\n", con.RemoteAddr())
+	fmt.Printf("Connected to %s\n", con.RemoteAddr())
+	scanner := bufio.NewScanner(con)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println("received:", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("read error:", err)
+	}
 }
